de09d7d8fb40451dfe8d85b91f8f4da839fcce3a
Takuto Wada <takuto.wada@gmail.com>
2015-01-06 02:00:36 +0900

    chore(espower-source): update copyright year

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * 
  * https://github.com/twada/espower-source
  *
- * Copyright (c) 2014 Takuto Wada
+ * Copyright (c) 2014-2015 Takuto Wada
  * Licensed under the MIT license.
  *   https://github.com/twada/espower-source/blob/master/MIT-LICENSE.txt
  */
