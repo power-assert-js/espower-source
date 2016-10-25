41bcc548af02e71dc6971dfc758219ae58f6a310
Takuto Wada <takuto.wada@gmail.com>
2016-10-25 16:57:06 +0900

    chore(package): introduce package-json-versionify
    to reduce content of package.json to be embedded into web bundle

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var glob = require("glob");
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var licensify = require('licensify');
+var packageJsonVersionify = require('package-json-versionify');
 var derequire = require('gulp-derequire');
 var dereserve = require('gulp-dereserve');
 var config = {
@@ -69,6 +70,7 @@ gulp.task('clean_test_bundle', function () {
 gulp.task('bundle', ['clean_bundle'], function() {
     var b = browserify({entries: config.bundle.srcFile, standalone: config.bundle.standalone});
     b.plugin(licensify);
+    b.transform(packageJsonVersionify, {global: true});
     return b.bundle()
         .pipe(source(config.bundle.destName))
         .pipe(dereserve())
