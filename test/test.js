78a57687b142ae99a0eae3566042a1467c016b1e
Takuto Wada <takuto.wada@gmail.com>
2016-11-12 00:53:41 +0900

    feat(package): upgrade espower to 2.0.0

--- test/test.js
+++ test/test.js
@@ -11,7 +11,6 @@ describe('with default options', function() {
         this.input = fs.readFileSync('test/fixtures/example.js', 'utf8'),
         this.output = espowerSource(this.input, this.path);
         this.map = convert.fromSource(this.output).toObject();
-        this.expected = fs.readFileSync('test/expected/example.js', 'utf8');
     });
 
     it('should return a string', function() {
@@ -19,16 +18,23 @@ describe('with default options', function() {
     });
 
     it('should transform source', function() {
-        assert.equal(this.output, this.expected);
+        var expected = fs.readFileSync('test/expected/example.js', 'utf8');
+        assert.equal(this.output, expected);
     });
 
     describe('generated sourceMap', function() {
         testSourceMapCommonAttributes();
         it('names', function () {
-            assert.deepEqual(this.map.names, ["assert","require","truthy","falsy","_expr","_capt","content","filepath","line","equal"]);
+            assert.deepEqual(this.map.names, [
+                "_PowerAssertRecorder1", "PowerAssertRecorder", "captured", "prototype",
+                "_capt", "value", "espath", "push",
+                "_expr", "source", "powerAssertContext", "events",
+                "_rec1", "_rec2", "_rec3",
+                "assert", "require", "truthy", "falsy", "content", "filepath", "line", "equal"
+            ]);
         });
         it('mappings', function () {
-            assert.equal(this.map.mappings, 'AAAA,IAAIA,MAAA,GAASC,OAAA,CAAQ,cAAR,CAAb,EACIC,MAAA,GAAS,MADb,EAEIC,KAAA,GAAQ,OAFZ;AAGAH,MAAA,CAAOA,MAAA,CAAAI,KAAA,CAAAJ,MAAA,CAAAK,KAAA,CAAAF,KAAA;AAAA,IAAAG,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAP,EAHA;AAIAR,MAAA,CAAOS,KAAP,CAAaT,MAAA,CAAAI,KAAA,CAAAJ,MAAA,CAAAK,KAAA,CAAAH,MAAA;AAAA,IAAAI,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAb,EAAqBR,MAAA,CAAAI,KAAA,CAAAJ,MAAA,CAAAK,KAAA,CAAAF,KAAA;AAAA,IAAAG,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAArB');
+            assert.equal(this.map.mappings, 'AAAA,IAAAA,qBAAA;AAAA,aAAAC,mBAAA;AAAA,aAAAC,QAAA;AAAA;AAAA,IAAAD,mBAAA,CAAAE,SAAA,CAAAC,KAAA,YAAAA,KAAA,CAAAC,KAAA,EAAAC,MAAA;AAAA,aAAAJ,QAAA,CAAAK,IAAA;AAAA,YAAAF,KAAA,EAAAA,KAAA;AAAA,YAAAC,MAAA,EAAAA,MAAA;AAAA;AAAA,eAAAD,KAAA;AAAA;AAAA,IAAAJ,mBAAA,CAAAE,SAAA,CAAAK,KAAA,YAAAA,KAAA,CAAAH,KAAA,EAAAI,MAAA;AAAA;AAAA,YAAAC,kBAAA;AAAA,gBAAAL,KAAA,EAAAA,KAAA;AAAA,gBAAAM,MAAA,OAAAT,QAAA;AAAA;AAAA,YAAAO,MAAA,EAAAA,MAAA;AAAA;AAAA;AAAA,WAAAR,mBAAA;AAAA;AAGO,IAAAW,KAAA,OAAAZ,qBAAA,GAHP;AAIa,IAAAa,KAAA,OAAAb,qBAAA,GAJb;AAIqB,IAAAc,KAAA,OAAAd,qBAAA,GAJrB;AAAA,IAAIe,MAAA,GAASC,OAAA,CAAQ,cAAR,CAAb,EACIC,MAAA,GAAS,MADb,EAEIC,KAAA,GAAQ,OAFZ;AAGAH,MAAA,CAAOH,KAAA,CAAAJ,KAAA,CAAAI,KAAA,CAAAR,KAAA,CAAAc,KAAA;AAAA,IAAAC,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAP,EAHA;AAIAN,MAAA,CAAOO,KAAP,CAAaT,KAAA,CAAAL,KAAA,CAAAK,KAAA,CAAAT,KAAA,CAAAa,MAAA;AAAA,IAAAE,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAb,EAAqBP,KAAA,CAAAN,KAAA,CAAAM,KAAA,CAAAV,KAAA,CAAAc,KAAA;AAAA,IAAAC,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAArB');
         });
     });
 
@@ -42,20 +48,20 @@ describe('with default options', function() {
             this.mappings = mappings;
         });
         it('mapping count', function () {
-            assert.equal(this.mappings.length, 86);
+            assert.equal(this.mappings.length, 186);
         });
         it('mapping with names', function () {
             var withNames = this.mappings.filter(function (mapping) { return mapping.name; });
-            assert.equal(withNames.length, 31);
+            assert.equal(withNames.length, 67);
         });
         it('originalPosition', function () {
-            assert.deepEqual(this.consumer.originalPositionFor({line:1,column:4}),
+            assert.deepEqual(this.consumer.originalPositionFor({line:26,column:4}),
                              {source:'test/fixtures/example.js',line:1,column:4,name:'assert'});
-            assert.deepEqual(this.consumer.originalPositionFor({line:1,column:38}),
+            assert.deepEqual(this.consumer.originalPositionFor({line:26,column:38}),
                              {source:'test/fixtures/example.js',line:2,column:4,name:'truthy'});
-            assert.deepEqual(this.consumer.originalPositionFor({line:1,column:55}),
+            assert.deepEqual(this.consumer.originalPositionFor({line:26,column:55}),
                              {source:'test/fixtures/example.js',line:3,column:4,name:'falsy'});
-            assert.deepEqual(this.consumer.originalPositionFor({line:7,column:7}),
+            assert.deepEqual(this.consumer.originalPositionFor({line:32,column:7}),
                              {source:'test/fixtures/example.js',line:5,column:7,name:'equal'});
         });
     });
@@ -74,7 +80,6 @@ describe('with customized options', function() {
             ]
         });
         this.map = convert.fromSource(this.output).toObject();
-        this.expected = fs.readFileSync('test/expected/customized.js', 'utf8');
     });
 
     it('should return a string', function() {
@@ -82,16 +87,23 @@ describe('with customized options', function() {
     });
 
     it('should transform source', function() {
-        assert.equal(this.output, this.expected);
+        var expected = fs.readFileSync('test/expected/customized.js', 'utf8');
+        assert.equal(this.output, expected);
     });
 
     describe('generated sourceMap', function() {
         testSourceMapCommonAttributes();
         it('names', function () {
-            assert.deepEqual(this.map.names, ["empower","require","formatter","busterAssertions","refute","targetMethods","oneArg","twoArgs","truthy","falsy","_expr","_capt","content","filepath","line","isNull","same"]);
+            assert.deepEqual(this.map.names, [
+                "_PowerAssertRecorder1", "PowerAssertRecorder", "captured", "prototype",
+                "_capt", "value", "espath", "push",
+                "_expr", "source", "powerAssertContext", "events",
+                "_rec1", "_rec2", "_rec3", "_rec4",
+                "empower","require","formatter","busterAssertions","refute","targetMethods","oneArg","twoArgs","truthy","falsy","content","filepath","line","isNull","same"
+            ]);
         });
         it('mappings', function () {
-            assert.equal(this.map.mappings, 'AAAA,IAAIA,OAAA,GAAUC,OAAA,CAAQ,SAAR,CAAd,EACIC,SAAA,GAAYD,OAAA,CAAQ,wBAAR,CADhB,EAEIE,gBAAA,GAAmBF,OAAA,CAAQ,mBAAR,CAFvB,EAGIG,MAAA,GAASJ,OAAA,CAAQG,gBAAA,CAAiBC,MAAzB,EAAiCF,SAAA,EAAjC,EAA8C;AAAA,QAAEG,aAAA,EAAe;AAAA,YAAEC,MAAA,EAAQ,CAAC,QAAD,CAAV;AAAA,YAAsBC,OAAA,EAAS,CAAC,MAAD,CAA/B;AAAA,SAAjB;AAAA,KAA9C,CAHb,EAIIC,MAAA,GAAS,MAJb,EAKIC,KAAA,GAAQ,OALZ;AAMAL,MAAA,CAAOA,MAAA,CAAAM,KAAA,CAAAN,MAAA,CAAAO,KAAA,CAAAH,MAAA;AAAA,IAAAI,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAP,EANA;AAOAV,MAAA,CAAOW,MAAP,CAAcX,MAAA,CAAAM,KAAA,CAAAN,MAAA,CAAAO,KAAA,CAAAF,KAAA;AAAA,IAAAG,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAd,EAPA;AAQAV,MAAA,CAAOY,IAAP,CAAYZ,MAAA,CAAAM,KAAA,CAAAN,MAAA,CAAAO,KAAA,CAAAH,MAAA;AAAA,IAAAI,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAZ,EAAoBV,MAAA,CAAAM,KAAA,CAAAN,MAAA,CAAAO,KAAA,CAAAF,KAAA;AAAA,IAAAG,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAApB');
+            assert.equal(this.map.mappings, 'AAAA,IAAAA,qBAAA;AAAA,aAAAC,mBAAA;AAAA,aAAAC,QAAA;AAAA;AAAA,IAAAD,mBAAA,CAAAE,SAAA,CAAAC,KAAA,YAAAA,KAAA,CAAAC,KAAA,EAAAC,MAAA;AAAA,aAAAJ,QAAA,CAAAK,IAAA;AAAA,YAAAF,KAAA,EAAAA,KAAA;AAAA,YAAAC,MAAA,EAAAA,MAAA;AAAA;AAAA,eAAAD,KAAA;AAAA;AAAA,IAAAJ,mBAAA,CAAAE,SAAA,CAAAK,KAAA,YAAAA,KAAA,CAAAH,KAAA,EAAAI,MAAA;AAAA;AAAA,YAAAC,kBAAA;AAAA,gBAAAL,KAAA,EAAAA,KAAA;AAAA,gBAAAM,MAAA,OAAAT,QAAA;AAAA;AAAA,YAAAO,MAAA,EAAAA,MAAA;AAAA;AAAA;AAAA,WAAAR,mBAAA;AAAA;AAMO,IAAAW,KAAA,OAAAZ,qBAAA,GANP;AAOc,IAAAa,KAAA,OAAAb,qBAAA,GAPd;AAQY,IAAAc,KAAA,OAAAd,qBAAA,GARZ;AAQoB,IAAAe,KAAA,OAAAf,qBAAA,GARpB;AAAA,IAAIgB,OAAA,GAAUC,OAAA,CAAQ,SAAR,CAAd,EACIC,SAAA,GAAYD,OAAA,CAAQ,wBAAR,CADhB,EAEIE,gBAAA,GAAmBF,OAAA,CAAQ,mBAAR,CAFvB,EAGIG,MAAA,GAASJ,OAAA,CAAQG,gBAAA,CAAiBC,MAAzB,EAAiCF,SAAA,EAAjC,EAA8C;AAAA,QAAEG,aAAA,EAAe;AAAA,YAAEC,MAAA,EAAQ,CAAC,QAAD,CAAV;AAAA,YAAsBC,OAAA,EAAS,CAAC,MAAD,CAA/B;AAAA,SAAjB;AAAA,KAA9C,CAHb,EAIIC,MAAA,GAAS,MAJb,EAKIC,KAAA,GAAQ,OALZ;AAMAL,MAAA,CAAOR,KAAA,CAAAJ,KAAA,CAAAI,KAAA,CAAAR,KAAA,CAAAoB,MAAA;AAAA,IAAAE,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAP,EANA;AAOAR,MAAA,CAAOS,MAAP,CAAchB,KAAA,CAAAL,KAAA,CAAAK,KAAA,CAAAT,KAAA,CAAAqB,KAAA;AAAA,IAAAC,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAd,EAPA;AAQAR,MAAA,CAAOU,IAAP,CAAYhB,KAAA,CAAAN,KAAA,CAAAM,KAAA,CAAAV,KAAA,CAAAoB,MAAA;AAAA,IAAAE,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAAZ,EAAoBb,KAAA,CAAAP,KAAA,CAAAO,KAAA,CAAAX,KAAA,CAAAqB,KAAA;AAAA,IAAAC,OAAA;AAAA,IAAAC,QAAA;AAAA,IAAAC,IAAA;AAAA,EAApB');
         });
     });
 
@@ -105,26 +117,26 @@ describe('with customized options', function() {
             this.mappings = mappings;
         });
         it('mapping count', function () {
-            assert.equal(this.mappings.length, 161);
+            assert.equal(this.mappings.length, 267);
         });
         it('mapping with names', function () {
             var withNames = this.mappings.filter(function (mapping) { return mapping.name; });
-            assert.equal(withNames.length, 53);
+            assert.equal(withNames.length, 91);
         });
         it('originalPosition', function () {
-            assert.deepEqual(this.consumer.originalPositionFor({line:1,column:34}),
+            assert.deepEqual(this.consumer.originalPositionFor({line:27,column:34}),
                              {source:'test/fixtures/customized.js',line:2,column:4,name:'formatter'});
-            assert.deepEqual(this.consumer.originalPositionFor({line:1,column:81}),
+            assert.deepEqual(this.consumer.originalPositionFor({line:27,column:81}),
                              {source:'test/fixtures/customized.js',line:3,column:4,name:'busterAssertions'});
-            assert.deepEqual(this.consumer.originalPositionFor({line:1,column:130}),
+            assert.deepEqual(this.consumer.originalPositionFor({line:27,column:130}),
                              {source:'test/fixtures/customized.js',line:4,column:4,name:'refute'});
-            assert.deepEqual(this.consumer.originalPositionFor({line:6,column:8}),
+            assert.deepEqual(this.consumer.originalPositionFor({line:32,column:8}),
                              {source:'test/fixtures/customized.js',line:5,column:4,name:'truthy'});
-            assert.deepEqual(this.consumer.originalPositionFor({line:6,column:25}),
+            assert.deepEqual(this.consumer.originalPositionFor({line:32,column:25}),
                              {source:'test/fixtures/customized.js',line:6,column:4,name:'falsy'});
-            assert.deepEqual(this.consumer.originalPositionFor({line:12,column:7}),
+            assert.deepEqual(this.consumer.originalPositionFor({line:38,column:7}),
                              {source:'test/fixtures/customized.js',line:8,column:7,name:'isNull'});
-            assert.deepEqual(this.consumer.originalPositionFor({line:17,column:7}),
+            assert.deepEqual(this.consumer.originalPositionFor({line:43,column:7}),
                              {source:'test/fixtures/customized.js',line:9,column:7,name:'same'});
         });
     });
@@ -156,7 +168,6 @@ describe('incoming code with SourceMap comment', function() {
         this.input = fs.readFileSync('test/fixtures/with-sourcemap.js', 'utf8'),
         this.output = espowerSource(this.input, this.path);
         this.map = convert.fromSource(this.output).toObject();
-        this.expected = fs.readFileSync('test/expected/with-sourcemap.js', 'utf8');
     });
 
     it('should return a string', function() {
@@ -164,7 +175,8 @@ describe('incoming code with SourceMap comment', function() {
     });
 
     it('should transform source', function() {
-        assert.equal(this.output, this.expected);
+        var expected = fs.readFileSync('test/expected/with-sourcemap.js', 'utf8');
+        assert.equal(this.output, expected);
     });
 
     describe('generated sourceMap', function() {
@@ -224,7 +236,7 @@ describe('incoming code with SourceMap comment', function() {
             assert.deepEqual(this.map.names, []);
         });
         it('mappings', function () {
-            assert.equal(this.map.mappings, 'AAAA,IAAA,MAAA,EAAA,MAAA;AAAA,MAAA,GAAS,OAAA,CAAQ,cAAR,CAAT,CAAA;AAAA,MAAA,GAAA,YAAA;AAAA,IAGe,SAAA,MAAA,CAAC,IAAD,EAAO,GAAP,EAAA;AAAA,QACX,KAAC,IAAD,GAAQ,IAAR,CADW;AAAA,QAEX,KAAC,GAAD,GAAO,GAAP,CAFW;AAAA,KAHf;AAAA,kBAAA;AAAA,CAAA,EAAA,CAAA;AAAA,QAAA,CAOS,eAPT,EAO0B,YAAA;AAAA,IACxB,UAAA,CAAW,YAAA;AAAA,eACT,KAAC,KAAD,GAAS;AAAA,YACP,QADO;AAAA,YAEP,IAFO;AAAA,YAGP,IAHO;AAAA,YAIP,KAJO;AAAA,YAKP,IALO;AAAA,YAMP,SANO;AAAA,YAOP;AAAA,gBACE,QADF;AAAA,gBAEE,OAFF;AAAA,aAPO;AAAA,YAWP,EACE,MAAA,EAAQ,IADV,EAXO;AAAA,YAcP,GAdO;AAAA,YAeP,QAfO;AAAA,YAgBP,MAhBO;AAAA,YAiBH,IAAA,MAAA,CAAO,OAAP,EAAgB,CAAhB,CAjBG;AAAA,UADA;AAAA,KAAX,EADwB;AAAA,WAsBxB,EAAA,CAAG,MAAH,EAAW,YAAA;AAAA,QACT,IAAA,GAAA,EAAA,KAAA,CADS;AAAA,QACT,KAAA,GAAQ,KAAC,KAAD,CAAO,MAAP,GAAgB,CAAxB,CADS;AAAA,QAET,GAAA,GAAU,IAAA,MAAA,CAAO,KAAP,EAAc,CAAd,CAAV,CAFS;AAAA,eAGT,MAAA,CAAO,MAAA,CAAA,KAAA,CAAA,MAAA,CAAA,KAAA,CAAA,MAAA,CAAA,KAAA,CAAA,MAAA,CAAA,KAAA,CAAA,MAAA,CAAA,KAAA,MAAC,KAAD,oCAAO,MAAA,CAAA,KAAA,CAAA,KAAA,qCAAP,8BAAc,IAAd,0BAAsB,MAAA,CAAA,KAAA,CAAA,MAAA,CAAA,KAAA,CAAA,GAAA,8BAAI,IAAJ,sBAAtB;AAAA,YAAA,OAAA;AAAA,YAAA,QAAA;AAAA,YAAA,IAAA;AAAA,UAAP,EAHS;AAAA,KAAX,EAtBwB;AAAA,CAP1B');
+            assert.equal(this.map.mappings, 'AAAA,IAAA,qBAAA;AAAA,aAAA,mBAAA;AAAA,aAAA,QAAA;AAAA;AAAA,IAAA,mBAAA,CAAA,SAAA,CAAA,KAAA,YAAA,KAAA,CAAA,KAAA,EAAA,MAAA;AAAA,aAAA,QAAA,CAAA,IAAA;AAAA,YAAA,KAAA,EAAA,KAAA;AAAA,YAAA,MAAA,EAAA,MAAA;AAAA;AAAA,eAAA,KAAA;AAAA;AAAA,IAAA,mBAAA,CAAA,SAAA,CAAA,KAAA,YAAA,KAAA,CAAA,KAAA,EAAA,MAAA;AAAA;AAAA,YAAA,kBAAA;AAAA,gBAAA,KAAA,EAAA,KAAA;AAAA,gBAAA,MAAA,OAAA,QAAA;AAAA;AAAA,YAAA,MAAA,EAAA,MAAA;AAAA;AAAA;AAAA,WAAA,mBAAA;AAAA;AAAA,IAAA,MAAA,EAAA,MAAA;AAAA,MAAA,GAAS,OAAA,CAAQ,cAAR,CAAT,CAAA;AAAA,MAAA,GAAA,YAAA;AAAA,IAGe,SAAA,MAAA,CAAC,IAAD,EAAO,GAAP,EAAA;AAAA,QACX,KAAC,IAAD,GAAQ,IAAR,CADW;AAAA,QAEX,KAAC,GAAD,GAAO,GAAP,CAFW;AAAA,KAHf;AAAA,kBAAA;AAAA,CAAA,EAAA,CAAA;AAAA,QAAA,CAOS,eAPT,EAO0B,YAAA;AAAA,IACxB,UAAA,CAAW,YAAA;AAAA,eACT,KAAC,KAAD,GAAS;AAAA,YACP,QADO;AAAA,YAEP,IAFO;AAAA,YAGP,IAHO;AAAA,YAIP,KAJO;AAAA,YAKP,IALO;AAAA,YAMP,SANO;AAAA,YAOP;AAAA,gBACE,QADF;AAAA,gBAEE,OAFF;AAAA,aAPO;AAAA,YAWP,EACE,MAAA,EAAQ,IADV,EAXO;AAAA,YAcP,GAdO;AAAA,YAeP,QAfO;AAAA,YAgBP,MAhBO;AAAA,YAiBH,IAAA,MAAA,CAAO,OAAP,EAAgB,CAAhB,CAjBG;AAAA,UADA;AAAA,KAAX,EADwB;AAAA,WAsBxB,EAAA,CAAG,MAAH,EAAW,YAAA;AAAA,QAGF,IAAA,KAAA,OAAA,qBAAA,GAHE;AAAA,QACT,IAAA,GAAA,EAAA,KAAA,CADS;AAAA,QACT,KAAA,GAAQ,KAAC,KAAD,CAAO,MAAP,GAAgB,CAAxB,CADS;AAAA,QAET,GAAA,GAAU,IAAA,MAAA,CAAO,KAAP,EAAc,CAAd,CAAV,CAFS;AAAA,eAGT,MAAA,CAAO,KAAA,CAAA,KAAA,CAAA,KAAA,CAAA,KAAA,CAAA,KAAA,CAAA,KAAA,CAAA,KAAA,CAAA,KAAA,CAAA,KAAA,CAAA,KAAA,MAAC,KAAD,oCAAA,KAAO,CAAA,KAAA,CAAA,KAAA,qCAAP,8BAAc,IAAd,0BAAA,KAAsB,CAAA,KAAA,CAAtB,KAAsB,CAAA,KAAA,CAAA,GAAA,8BAAI,IAAJ,sBAAtB;AAAA,YAAA,OAAA;AAAA,YAAA,QAAA;AAAA,YAAA,IAAA;AAAA,UAAP,EAHS;AAAA,KAAX,EAtBwB;AAAA,CAP1B');
         });
     });
 
@@ -238,31 +250,31 @@ describe('incoming code with SourceMap comment', function() {
             this.mappings = mappings;
         });
         it('mapping count', function () {
-            assert.equal(this.mappings.length, 223);
+            assert.equal(this.mappings.length, 312);
         });
         it('mapping with names', function () {
             var withNames = this.mappings.filter(function (mapping) { return mapping.name; });
             assert.equal(withNames.length, 0);
         });
         it('originalPosition', function () {
-
+            // name
             assert.deepEqual(this.consumer.generatedPositionFor({
                 source:'/absolute/path/to/coffee_script_test.coffee',
                 line:4,
                 column:16
             }), {
-                line:4,
+                line:26,
                 column:20,
                 lastColumn: null
             });
 
-            assert.deepEqual(this.consumer.originalPositionFor({line:4,column:4}),
+            assert.deepEqual(this.consumer.originalPositionFor({line:26,column:4}),
                              {source:'/absolute/path/to/coffee_script_test.coffee',line:4,column:15,name:null});
 
-            assert.deepEqual(this.consumer.originalPositionFor({line:32,column:8}),
+            assert.deepEqual(this.consumer.originalPositionFor({line:54,column:8}),
                              {source:'/absolute/path/to/coffee_script_test.coffee',line:31,column:4,name:null});
 
-            assert.deepEqual(this.consumer.originalPositionFor({line:34,column:15}),
+            assert.deepEqual(this.consumer.originalPositionFor({line:57,column:15}),
                              {source:'/absolute/path/to/coffee_script_test.coffee',line:33,column:4,name:null});
         });
     });
@@ -312,7 +324,7 @@ describe('when filepath is not specified', function () {
         });
         it('no filepath and sourcemap in output', function () {
             var expected = fs.readFileSync('test/expected/example_without_filepath_and_sourcemap.js', 'utf8');
-            assert.equal(this.output + '\n', expected);
+            assert.equal(this.output, expected);
         });
         it('sourcemap will not be attached', function () {
             var map = convert.fromSource(this.output);
